a76bd7bc90c4174f39e1605c17f0ffe96304d439
gostok <stiv4ik12012@gmail.com>
2024-12-10 14:58:02 +0300

    the architecture and logic of the project have been updated

--- main.py
+++ main.py
@@ -1,129 +1,13 @@
 from fastapi import FastAPI
 from fastapi.routing import APIRouter
-from fastapi import HTTPException
-
-import uuid
-import re
 import uvicorn
 
-from sqlalchemy import Column, Boolean, String
-from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
-from sqlalchemy.orm import sessionmaker, declarative_base
-from sqlalchemy.dialects.postgresql import UUID
-
-from pydantic import BaseModel, EmailStr, field_validator
-
-import settings
-
-
-# DATABASE
-
-engine = create_async_engine(settings.REAL_DATABASE_URL, future=True, echo=True)
-
-async_session = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
-
-
-# DATABASE MODELS
-
-Base = declarative_base()
-
-
-class User(Base):
-    __tablename__ = "users"
-
-    user_id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    name = Column(String, nullable=False)
-    surname = Column(String, nullable=False)
-    email = Column(String, nullable=False, unique=True)
-    is_active = Column(Boolean(), default=True)
-
-
-# DATABASE IN BUSINESS CONTEXT
-
-
-class UserDAL:
-    def __init__(self, db_session: AsyncSession):
-        self.db_session = db_session
-
-    async def create_user(self, name: str, surname: str, email: str) -> User:
-        new_user = User(
-            name=name,
-            surname=surname,
-            email=email,
-        )
-        self.db_session.add(new_user)
-        await self.db_session.flush()
-        return new_user
-
-
-# API MODELS
-
-LATTER_MATCH_PATTERN = re.compile(r"^[а-яА-Яa-zA-Z\-]+$")
-
-
-class TunedModel(BaseModel):
-    class Config:
-        orm_mode = True
-
-
-class ShowUser(TunedModel):
-    user_id: uuid.UUID
-    name: str
-    surname: str
-    email: EmailStr
-    is_active: bool
-
-
-class UserCreate(BaseModel):
-    name: str
-    surname: str
-    email: EmailStr
-
-    @field_validator("name")
-    def validate_name(cls, value):
-        if not LATTER_MATCH_PATTERN.match(value):
-            raise HTTPException(
-                status_code=422, detail="Name should contains only letters"
-            )
-        return value
-
-    @field_validator("surname")
-    def validate_surname(cls, value):
-        if not LATTER_MATCH_PATTERN.match(value):
-            raise HTTPException(
-                status_code=422, detail="Surname should contains only letters"
-            )
-        return value
+from api.handlers import user_router
 
 
 # API ROUTERS
 
 app = FastAPI(title="practice-university")
-
-user_router = APIRouter()
-
-
-async def _create_new_user(body: UserCreate) -> ShowUser:
-    async with async_session() as session:
-        async with session.begin():
-            user_dal = UserDAL(session)
-            user = await user_dal.create_user(
-                name=body.name,
-                surname=body.surname,
-                email=body.email,
-            )
-            return ShowUser(
-                user_id=user.user_id,
-                name=user.name,
-                surname=user.surname,
-                email=user.email,
-                is_active=user.is_active,
-            )
-
-
-@user_router.post("/", response_model=ShowUser)
-async def create_user(body: UserCreate) -> ShowUser:
-    return await _create_new_user(body=body)
 
 
 main_api_router = APIRouter()
@@ -132,4 +16,4 @@ main_api_router.include_router(user_router, prefix="/user", tags=["user"])
 app.include_router(main_api_router)
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="127.0.0.1", port=8080)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
